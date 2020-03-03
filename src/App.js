ba04c7c0efbbc347d691e52af0e4ae206239ee27
Ida Lindell <ida.lindahl@gmail.com>
2020-03-03 20:17:55 +0100

    text update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Firebase and Travis!
+          Hello Firebase and Travis! Here we come
         </p>
         <a
           className="App-link"
